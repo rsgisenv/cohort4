cd99f03eb7c2499dafb2d4b13f2517135a4ac04e
LAPTOP-CJC93NMP\Hamid <rsgisenv@gmail.com>
2020-02-24 14:19:41 -0700

    created syntax

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -4,7 +4,8 @@ const functions = {
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num < 101) return "large";
+        return "extra large"
     },
 
     add: (num1, num2) => {
@@ -13,7 +14,16 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
+    },
+    isEven: (num) => {
+        if (num % 2==0) 
+        return true;
+        return false;
+    },
+
+
+
+    
 
     
 };
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -6,10 +6,14 @@ test('Check the sizes', () => {
     expect(functions.size(10)).toBe("medium");
     expect(functions.size(15)).toBe("medium");
     expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+    expect(functions.size(101)).toBe("extra large");
 });
 
 test('Does that add function work?', () => {
     expect(functions.add(1,2)).toBe(3);
     expect(functions.add(101,202)).toBe(303);
 });
+test('Does isEven function work?', () =>{
+    expect(functions.isEven(1)).toBe(false);
+    expect(functions.isEven(6)).toBe(true);
+})
